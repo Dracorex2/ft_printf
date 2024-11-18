4151982a819e50c150dd009f3a0d4c7c3efc819d
Dracorex2 <dracorex2.mail@gmail.com>
2024-11-18 14:24:11 +0100

    nbr

--- utils4.c
+++ utils4.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils4.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lucmansa <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: lucmansa <lucmansa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 15:11:06 by lucmansa          #+#    #+#             */
-/*   Updated: 2024/11/15 17:43:12 by lucmansa         ###   ########.fr       */
+/*   Updated: 2024/11/18 14:24:00 by lucmansa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,5 @@ int ft_putnstr(char *str, int n)
 	len = 0;
 	while (n--)
 		write(1, &str[len++], 1);
-	return (len);
-}
-
-int ft_null(t_flag flag)
-{
-	int	len;
-
-	len = 0;
-	if (flag.first)
-		len += ft_space(flag.first);
-	else
-		len += 0;
 	return (len);
 }
